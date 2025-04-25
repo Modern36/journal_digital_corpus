bcbb9195318e778b4043deaf5e83a436d205b4b8
M~Johansson <MathiasJoha@gmail.com>
2025-04-25 14:36:58 +0200

    Rename name-year-mapping variable

--- src/journal_digital_corpus/settings.py
+++ src/journal_digital_corpus/settings.py
@@ -8,5 +8,5 @@ load_dotenv()
 video_root = Path(os.getenv("JOURNAL_DIGITALROOT"))
 project_root = Path(__file__).parents[2]
 corpus_root = project_root / "corpus"
-metadata_file = project_root / "name_year.tsv"
+name_year_mapping = project_root / "name_year.tsv"
 empty_srts_file = project_root / "empty.tsv"
