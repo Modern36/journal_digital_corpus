41016383217bda34bd4ee5fe3c87a891e8813e0e
M~Johansson <MathiasJoha@gmail.com>
2025-04-25 14:40:26 +0200

    Add path to measurements file

--- src/journal_digital_corpus/settings.py
+++ src/journal_digital_corpus/settings.py
@@ -10,3 +10,5 @@ project_root = Path(__file__).parents[2]
 corpus_root = project_root / "corpus"
 name_year_mapping = project_root / "name_year.tsv"
 empty_srts_file = project_root / "empty.tsv"
+
+measurements = corpus_root / "measurements.tsv"
