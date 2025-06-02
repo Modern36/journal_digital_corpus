0458263893f15c89ac70c26c23260269e2045bc7
M~Johansson <MathiasJoha@gmail.com>
2025-06-02 18:23:13 +0200

    Move asr-corpus to ./corpus/speech/

--- src/journal_digital_corpus/settings.py
+++ src/journal_digital_corpus/settings.py
@@ -8,13 +8,14 @@ load_dotenv()
 video_root = Path(os.getenv("JOURNAL_DIGITALROOT"))
 project_root = Path(__file__).parents[2]
 corpus_root = project_root / "corpus"
+speech_root = corpus_root / "speech"
 
 name_year_mapping = project_root / "name_year.tsv"
 name_seconds_mapping = project_root / "name_seconds.tsv"
 empty_srts_file = project_root / "empty.tsv"
 
-measurements = corpus_root / "measurements.tsv"
-measurements_description = corpus_root / "measurements_description.tsv"
+measurements = speech_root / "measurements.tsv"
+measurements_description = speech_root / "measurements_description.tsv"
 
 
-measurements_sum = corpus_root / "measurements_sum.tsv"
+measurements_sum = speech_root / "measurements_sum.tsv"
