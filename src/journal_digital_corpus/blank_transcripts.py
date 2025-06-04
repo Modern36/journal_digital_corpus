58ccc8c1362c218b0a8b303c8c76d05b0828b4e8
M~Johansson <MathiasJoha@gmail.com>
2025-06-04 19:13:50 +0200

    Make empty-srt detector more generic.

--- src/journal_digital_corpus/blank_transcripts.py
+++ src/journal_digital_corpus/blank_transcripts.py
@@ -1,35 +1,39 @@
-from journal_digital_corpus.settings import corpus_root, empty_srts_file
+from pathlib import Path
+
+from settings import intertitle_root, speech_root
 
 
-def emtpy_srt():
-    for srt in corpus_root.glob("**/*.srt"):
+def emtpy_srt(corpus_dir):
+    for srt in corpus_dir.glob("**/*.srt"):
         with open(srt, "r", encoding="utf8") as f:
             text = f.read()
         if text.strip() == "":
             yield srt
 
 
-def load_empty_filenames():
+def load_empty_filenames(empty_srts_file):
     if not empty_srts_file.exists():
         return set()
     with open(empty_srts_file, "r", encoding="utf8") as f:
         return {line.strip() for line in f.readlines()}
 
 
-def write_empty_filenames(empyt_ids_set: list):
+def write_empty_filenames(empty_srts_file, empyt_ids_set: list):
     with open(empty_srts_file, "w", encoding="utf8") as f:
         f.write("\n".join(empyt_ids_set))
 
 
-def remove_empty_transcripts():
-    empty_ids = load_empty_filenames() | {
-        empty_srt.name.replace(".srt", "") for empty_srt in emtpy_srt()
+def remove_empty_transcripts(corpus_dir: Path):
+    empty_srts_file = corpus_dir / "empty.tsv"
+    empty_ids = load_empty_filenames(empty_srts_file) | {
+        empty_srt.name.replace(".srt", "")
+        for empty_srt in emtpy_srt(corpus_dir)
     }
 
-    write_empty_filenames(sorted(empty_ids))
+    write_empty_filenames(empty_srts_file, sorted(empty_ids))
 
     for empty_id in empty_ids:
-        files = list(corpus_root.glob(f"**/{empty_id}.srt"))
+        files = list(corpus_dir.glob(f"**/{empty_id}.srt"))
         assert len(files) in {0, 1}
 
         if not files:
@@ -42,3 +46,8 @@ def remove_empty_transcripts():
             file.parent.rmdir()
             if len(list(file.parents[1].iterdir())) == 0:
                 file.parents[1].rmdir()
+
+
+if __name__ == "__main__":
+    for sub_corpus in (speech_root, intertitle_root):
+        remove_empty_transcripts(sub_corpus)
